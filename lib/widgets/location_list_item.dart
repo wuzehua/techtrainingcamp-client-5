d131e32b162e9206f066ad3e7c702dac87bc4416
rainzhong <rainzhong@tencent.com>
2020-08-17 21:14:24 +0800

    实现地区搜索

--- lib/widgets/location_list_item.dart
+++ lib/widgets/location_list_item.dart
@@ -1,17 +1,43 @@
+import 'package:clock_challenge/model/location_response_entity.dart';
 import 'package:flutter/material.dart';
 
 class LocationListItem extends StatelessWidget {
 
   final VoidCallback onPressed;
+  final Location location;
 
-  LocationListItem({Key key,
+  LocationListItem(this.location,
+                    {Key key,
                     this.onPressed})
       :super(key: key);
 
 
   @override
   Widget build(BuildContext context) {
-    return null;
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FlatButton(
+          child: Row(
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(location.name),
+                    Text('${location.country} ${location.adm1} ${location.adm2}'),
+                    Text('${location.lat} ${location.lon}')
+                  ],
+                ),
+              ),
+              Text('GMT${location.utcOffset}')
+            ],
+          ),
+          onPressed: onPressed,
+        ),
+      ),
+    );
   }
 
 }
--- lib/widgets/timezone_search_panel.dart
+++ lib/widgets/timezone_search_panel.dart
@@ -1,5 +1,6 @@
 import 'package:clock_challenge/model/location_response_entity.dart';
 import 'package:clock_challenge/network/http_service.dart';
+import 'package:clock_challenge/widgets/location_list_item.dart';
 import 'package:flutter/material.dart';
 
 class TimezoneSearchPanel extends StatefulWidget {
@@ -15,38 +16,46 @@ class TimezoneSearchPanel extends StatefulWidget {
 class _TimezoneSearchState extends State<TimezoneSearchPanel> {
 
   LocationResponse _response;
+  String _searchLocation;
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          RaisedButton(
-            child: Text('Test'),
-            onPressed: () async {
-              LocationResponse res = await HttpService.getService().getLocations('深圳');
-              setState(() {
-                _response = res;
-              });
-            },
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  onChanged: (value) {
+                    _searchLocation = value;
+                  },
+                ),
+              ),
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () async {
+                  LocationResponse res = await HttpService.getService().getLocations(_searchLocation);
+                  setState(() {
+                    _response = res;
+                  });
+                },
+              ),
+            ],
           ),
-          Container(
+          Expanded(
             child: ListView.builder(
                 itemCount: _itemCount(),
                 itemBuilder: (context, index) {
                   if (_response != null && _response.location != null) {
-                    return ListTile(
-                      title: Text('${_response.location[index].name}'),
-                      subtitle: Text('${_response.location[index].country}'),
-                      onTap: () {
+                    return LocationListItem(_response.location[index],
+                      onPressed: (){
 
                       },
                     );
                   }
                   return null;
                 }),
-            height: 200,
           )
           ,
         ],
